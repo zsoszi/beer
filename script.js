748884e42a7ac658b15babc24a12da9d2e8b60b8
zsoszi <zsolt.szilagyi86@gmail.com>
2021-04-10 16:25:48 +0200

    beer
    beer

---
+++ script.js
@@ -0,0 +1,9 @@
+function colorEnter() {
+    let text = document.getElementById("cls-10").style.fill = "green";
+    //console.log(text);
+}
+
+function colorLeave() {
+    let text = document.getElementById("cls-10").style.fill = "";
+    //console.log(text);
+}
